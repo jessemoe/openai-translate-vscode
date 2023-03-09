8b461a68a0960939dceef09e2721129c087b2f8b
stark-eth <1319114370@qq.com>
2023-03-09 12:17:53 +0800

    add request utils

--- src/translate.ts
+++ src/translate.ts
@@ -1,17 +1,9 @@
 import * as vscode from 'vscode';
 import { onTranslateSuccess } from './extension';
-import axios from 'axios';
+import { postApi } from './utils';
 
 export async function translate(textToTranslate: string, selection: vscode.Selection, language: string) {
-    const apiKey = vscode.workspace.getConfiguration('openaiTranslate')['apiKey'];
-    const apiURL = vscode.workspace.getConfiguration('openaiTranslate')['apiURL'];
-    const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${apiKey}`,
-    }
-    const config = {
-        headers: headers
-    };
+
     let systemPrompt = 'You are a translation engine that can only translate text and cannot interpret it.'
     let assistantPrompt = `translate to ${language}`
     const data = {
@@ -33,18 +25,15 @@ export async function translate(textToTranslate: string, selection: vscode.Selec
             { role: 'user', content: `${textToTranslate}` },
         ],
     }
+    await postApi(data, {
+        onMessage: (msg) => {
+            onTranslateSuccess(selection, language, msg);
+        },
+        onError: (err) => {
+            vscode.window.showErrorMessage(err);
+        }
+    })
 
-    axios.post(apiURL+'/v1/chat/completions', data, config)
-        .then(function (response) {
-            const targetTxt = response.data.choices[0].message.content
-            onTranslateSuccess(selection, language, targetTxt);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-
-
-    
 }
 
 
---
+++ src/utils.ts
@@ -0,0 +1,42 @@
+import axios from "axios";
+import * as vscode from 'vscode';
+
+interface PostOptions {
+    onMessage(data: string): void
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    onError(error: any): void
+}
+
+interface OpanaiBody {
+    model: string,
+    temperature: number,
+    max_tokens: number,
+    top_p: number,
+    frequency_penalty: number,
+    presence_penalty: number,
+    messages: {
+        role: string,
+        content: string,
+    }[],
+}
+
+export async function postApi(data:OpanaiBody, options:PostOptions) {
+    const apiKey = vscode.workspace.getConfiguration('openaiTranslate')['apiKey'];
+    const apiURL = vscode.workspace.getConfiguration('openaiTranslate')['apiURL'];
+    const { onMessage, onError } = options
+    const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`,
+    }
+    const config = {
+        headers: headers
+    };
+    axios.post(apiURL + '/v1/chat/completions', data, config)
+        .then(function (response) {
+            const targetTxt = response.data.choices[0].message.content
+            onMessage(targetTxt)
+        })
+        .catch(function (error) {
+            onError(error)
+        });
+}
